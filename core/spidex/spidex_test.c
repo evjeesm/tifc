053bfc595f06215378fdc566993a76f99caadba7
evjeesm <jekas.obps@gmail.com>
2025-11-26 18:01:28 +0200

    improving tests

--- core/spidex/spidex_test.c
+++ core/spidex/spidex_test.c
@@ -4,7 +4,17 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define TEST_ESC "\x1b"
+#define TEST_COLOR_RED TEST_ESC "[31m"
+#define TEST_COLOR_CLEAR TEST_ESC "[0m"
+
+#define RED(msg) TEST_COLOR_RED msg TEST_COLOR_CLEAR
+
 #define RUN_TEST(s,t,c) do { if (!run_test(#t,s,t,c)) { exit(1); }} while(0)
+#define TEST_MSG(msg, ...) fprintf(stderr, msg "\n" ,##__VA_ARGS__)
+#define TEST_ASSERT(cond, msg) do { if (!(cond)) { TEST_MSG(msg); return false; } } while (0)
+
+#define TEST_SUCCESS() return true
 
 typedef void (*setup_func_t) (spidex_t *const spidex);
 typedef bool (*test_func_t) (spidex_t *const spidex);
@@ -27,17 +37,17 @@ static ssize_t value_cmp(const void *const value, const void *const element, voi
 static bool test_empty(spidex_t *const spidex)
 {
     // CASE: EMPTY AFTER CREATION
-    if (!spidex_is_empty(spidex)) return false;
+    TEST_ASSERT(spidex_is_empty(spidex), RED("Spidex should be empty after creation!"));
 
     // CASE: NOT EMPTY AFTER ADD
     spidex_area_t area = {.start = {0, 0}, .end = {10, 10}};
     spidex_value_t value = NULL;
     spidex_add(spidex, &area, value);
 
-    if (spidex_is_empty(spidex)) return false;
+    TEST_ASSERT(!spidex_is_empty(spidex),
+                RED("Spidex should NOT be empty after spidex_add()!"));
 
-    // ALL CASES PASS
-    return true;
+    TEST_SUCCESS();
 }
 
 static bool test_has_intersect_single_area(spidex_t *const spidex)
@@ -48,72 +58,90 @@ static bool test_has_intersect_single_area(spidex_t *const spidex)
     spidex_add(spidex, &area, value);
 
     // CASE: INTERSECT WITH SAME AREA
-    if (!spidex_has_intersect(spidex, &area)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &area)),
+        RED("Intersect with area of same size should be detected!"));
 
     // CASE: LEFT ADJACENT
     spidex_area_t left_adjacent = {.start = {-10, 0}, .end = {0, 10}};
-    if (spidex_has_intersect(spidex, &left_adjacent)) return false;
+    TEST_ASSERT((!spidex_has_intersect(spidex, &left_adjacent)),
+        RED("No intersection should be detected with adjacent area from the left!"));
 
     // CASE: RIGHT ADJASENT
     spidex_area_t right_adjacent = {.start = {10, 0}, .end = {20, 10}};
-    if (spidex_has_intersect(spidex, &right_adjacent)) return false;
+    TEST_ASSERT((!spidex_has_intersect(spidex, &right_adjacent)),
+        RED("No intersection should be detected with adjacent area from the right!"));
 
     // CASE: TOP ADJACENT
     spidex_area_t top_adjacent = {.start = {0, -10}, .end = {10, 0}};
-    if (spidex_has_intersect(spidex, &top_adjacent)) return false;
+    TEST_ASSERT((!spidex_has_intersect(spidex, &top_adjacent)),
+        RED("No intersection should be detected with adjacent area from the top!"));
 
     // CASE: BOT ADJACENT
     spidex_area_t bot_adjacent = {.start = {0, 10}, .end = {10, 20}};
-    if (spidex_has_intersect(spidex, &bot_adjacent)) return false;
+    TEST_ASSERT((!spidex_has_intersect(spidex, &bot_adjacent)),
+        RED("No intersection should be detected with adjacent area from the bottom!"));
 
     // CASE: INTERSECT WITH LEFT ALIGNED SMALLER AREA
     spidex_area_t left_aligned_smaller = {.start = {0, 0}, .end = {5, 5}};
-    if (!spidex_has_intersect(spidex, &left_aligned_smaller)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &left_aligned_smaller)),
+        RED("Intersection should be detected with left aligned area of smaller size!"));
 
     // CASE: INTERSECT WITH INNER AREA
     spidex_area_t inner = {.start = {1, 1}, .end = {5, 5}};
-    if (!spidex_has_intersect(spidex, &inner)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &inner)),
+        RED("Intersection should be detected with inner area!"));
 
     // CASE: INTERSECT WITH RIGHT ALIGNED SMALLER AREA
     spidex_area_t right_aligned_smaller = {.start = {5, 5}, .end = {10, 10}};
-    if (!spidex_has_intersect(spidex, &right_aligned_smaller)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &right_aligned_smaller)),
+        RED("Intersection should be detected with right aligned area of smaller size!"));
 
     // CASE: INTERSECT WITH LEFT ALIGNED BIGGER AREA
     spidex_area_t left_aligned_bigger = {.start = {0, 0}, .end = {15, 15}};
-    if (!spidex_has_intersect(spidex, &left_aligned_bigger)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &left_aligned_bigger)),
+        RED("Intersection should be detected with left aligned area of bigger size!"));
 
     // CASE: INTERSECT WITH OUTTER AREA
     spidex_area_t outer = {.start = {-1, -1}, .end = {11, 11}};
-    if (!spidex_has_intersect(spidex, &outer)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &outer)),
+        RED("Intersection should be detected with outer area!"));
 
     // CASE: INTERSECT WITH RIGHT ALIGNED BIGGER AREA
     spidex_area_t right_aligned_bigger = {.start = {-5, -5}, .end = {10, 10}};
-    if (!spidex_has_intersect(spidex, &right_aligned_bigger)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &right_aligned_bigger)),
+        RED("Intersection should be detected with right aligned area of bigger size!"));
 
     // CASE: TOP LEFT AREA
     spidex_area_t top_left = {.start = {-5, -5}, .end = {5, 5}};
-    if (!spidex_has_intersect(spidex, &top_left)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &top_left)),
+        RED("Intersection should be detected with top left shifted area!"));
 
     // CASE: TOP RIGHT
     spidex_area_t top_right = {.start = {5, -5}, .end = {15, 5}};
-    if (!spidex_has_intersect(spidex, &top_right)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &top_right)),
+        RED("Intersection should be detected with top right shifted area!"));
 
     // CASE: BOT LEFT
     spidex_area_t bot_left = {.start = {-5, 5}, .end = {5, 15}};
-    if (!spidex_has_intersect(spidex, &bot_left)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &bot_left)),
+        RED("Intersection should be detected with bottom left shifted area!"));
 
     // CASE: BOT RIGHT
     spidex_area_t bot_right = {.start = {5, 5}, .end = {15, 15}};
-    if (!spidex_has_intersect(spidex, &bot_right)) return false;
+    TEST_ASSERT((spidex_has_intersect(spidex, &bot_right)),
+        RED("Intersection should be detected with bottom right shifted area!"));
 
-    // ALL CASES PASS
-    return true;
+    TEST_SUCCESS();
 }
 
 bool test_has_intersect_many_areas(spidex_t *const spidex)
 {
-    UNUSED(spidex);
-    return true;
+    spidex_area_t middle = { .start = {2, 2}, .end = {8, 8} };
+
+    TEST_ASSERT((!spidex_has_intersect(spidex, &middle)),
+        RED("Intersect should not be detected with middle cut-out area!"));
+
+    TEST_SUCCESS();
 }
 
 int main(void)
@@ -168,7 +196,8 @@ static bool run_test(const char *const test_name, setup_func_t setup_func, test_
     setup_func(&spidex);
 
     bool result = test_func(&spidex);
-    if (!result) fprintf(stderr, "TEST %s FAILED!\n", test_name);
+    if (!result) TEST_MSG("["RED("FAILED")"] Test %s\n", test_name);
+
 
     cleanup_func(&spidex);
     return result;
